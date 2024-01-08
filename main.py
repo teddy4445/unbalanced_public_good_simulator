7b7ef2341656080750727b6ed84ad79353f35cb3
Teddy Lazebnik <lazebnik.teddy@gmail.com>
2024-01-08 09:19:48 +0200

    final commit

--- main.py
+++ main.py
@@ -24,6 +24,22 @@ def moving_average(a: np.array,
     return ret[n - 1:] / n
 
 
+def zscore(s,
+           window,
+           thresh=1):
+    try:
+        roll = s.rolling(window=window, min_periods=1, center=True)
+    except:
+        s = pd.Series(s)
+        roll = s.rolling(window=window, min_periods=1, center=True)
+    avg = roll.mean()
+    std = roll.std(ddof=0)
+    z = s.sub(avg).div(std)
+    m = z.between(-thresh, thresh)
+    s = s.where(m, avg)
+    return np.array(s)
+
+
 class Main:
     """
     The main class of the project
@@ -53,11 +69,11 @@ class Main:
         # make sure the IO is file
         Main.io()
         # baseline graphs
-        Main.first_plot()
+        #Main.first_plot()
         # one-dim sensitivity graphs
         #Main.second_graph()
         # heatmap sensitivity graphs
-        #Main.third_graph()
+        Main.third_graph()
 
     @staticmethod
     def io() -> None:
@@ -68,21 +84,21 @@ class Main:
                 pass
 
     @staticmethod
-    def desire_metric(df: dict) -> tuple:
+    def desire_metric(df: dict) -> int:
         """
         An approximation to the basic reproduction number
         """
-        score = [abs(df["r"][index] / (df["p"][index] + df["r"][index])) for index in range(min([len(df["r"]), len(df["p"])]))]
-        return np.mean(score), np.std(score)
+        diff = [df["r"][index+1]-df["r"][index] for index in range(len(df["r"])-1)]
+        for index in range(len(diff)-1):
+            if diff[index] > 0 and diff[index+1] < 0:
+                return index
+        return len(df["r"])
 
     @staticmethod
     def first_plot() -> None:
         # baseline graph - just run the model and plot it
         initial_conditions = [
-            [900, 100],
-            [100, 900],
-            [500, 500],
-            [8000, 1000]
+            [3910000, 233000]
         ]
         for index, initial_condition in enumerate(initial_conditions):
             print("Main.first_plot: baseline for initial condition: {} (#{}/{}-{:.2f}%)".format(initial_condition,
@@ -90,23 +106,34 @@ class Main:
                                                                                                 len(initial_conditions),
                                                                                                 (index + 1) * 100 / len(
                                                                                                     initial_conditions)))
-            Plotter.baseline(model_matrix=Main.solve_the_model_forward_euler(initial_condition=initial_condition,
-                                                                             gamma=0.4,
-                                                                             tau2=0.1,
-                                                                             mu=100,
-                                                                             ),
-                             save_path=os.path.join(Main.RESULTS_FOLDER, "baseline_{}.pdf".format(index)))
+            Plotter.baseline(model_matrix=Main.solve_the_model_forward_euler(initial_condition=initial_condition),
+                             save_path=os.path.join(Main.RESULTS_FOLDER, "baseline_{}.pdf".format(index+1)))
+            Plotter.baseline_profit(model_matrix=Main.solve_the_model_forward_euler(initial_condition=initial_condition,
+                                                                                    show_profit=True),
+                                    save_path=os.path.join(Main.RESULTS_FOLDER, "baseline_profit_{}.pdf".format(index+1)))
 
     @staticmethod
     def second_graph() -> None:
         """
         Generate all the parameter sensitivity graphs
         """
-        Main.sens(parameter_range=[0.2 * i for i in range(11)], parameter_name="beta")
+        Main.sens(parameter_range=[5.16e-09 * (0.5 + 0.1 * i) for i in range(11)], parameter_name="alpha1")
 
-        Main.sens(parameter_range=[1000 * (i + 1) for i in range(11)], parameter_name="zeta")
+        Main.sens(parameter_range=[1.76e-08 * (0.5 + 0.1 * i) for i in range(11)], parameter_name="alpha2")
 
-        Main.sens(parameter_range=[0.02 * i for i in range(11)], parameter_name="gamma")
+        Main.sens(parameter_range=[10000000 * (0.5 + 0.1 * i) for i in range(11)], parameter_name="zeta")
+
+        Main.sens(parameter_range=[1350 * (0.5 + 0.1 * i)for i in range(11)], parameter_name="gamma")
+
+        Main.sens(parameter_range=[15200 * (0.5 + 0.1 * i)for i in range(11)], parameter_name="psi1")
+
+        Main.sens(parameter_range=[1170 * (0.5 + 0.1 * i)for i in range(11)], parameter_name="tau1")
+
+        Main.sens(parameter_range=[0.33 * (0.5 + 0.1 * i)for i in range(11)], parameter_name="beta1")
+
+        Main.sens(parameter_range=[5.28 * (0.5 + 0.1 * i)for i in range(11)], parameter_name="beta2")
+
+        Main.sens(parameter_range=[0.000000000000318 * (0.5 + 0.1 * i)for i in range(11)], parameter_name="mu")
 
     @staticmethod
     def sens(parameter_range: list,
@@ -123,38 +150,41 @@ class Main:
                                                                               len(parameter_range),
                                                                               (index + 1) * 100 / len(parameter_range)))
             values = [Main.desire_metric(df=Main.solve_the_model_wrapper(params={parameter_name: parm_val},
-                                                                         initial_condition=[random.randint(10, 100),
-                                                                                            random.randint(1, 100)]))[0]
+                                                                         initial_condition=[3430000, 485000]))
                       for _ in range(Main.RANDOM_SAMPLES)]
-            ans_mean.append(np.mean(values))
-            ans_std.append(np.std(values))
+            ans_mean.append(np.mean(values)/200)
+            ans_std.append(np.std(values)/Main.RANDOM_SAMPLES)
         Plotter.sensitivity(x=parameter_range,
                             y=ans_mean,
                             y_err=ans_std,
-                            x_label=parameter_name,
-                            y_label="$r(t)/(r(t) + p(t))$",
-                            save_path=os.path.join(Main.RESULTS_FOLDER, "sensitivity_{}.pdf".format(parameter_name)))
+                            x_label="\\{}".format(parameter_name),
+                            y_label="$M$",
+                            save_path=os.path.join(Main.RESULTS_FOLDER, "sens_{}.pdf".format(parameter_name)))
 
     @staticmethod
     def third_graph() -> None:
         """
         This function responsible to run all the needed heatmap analysis needed for the paper
         """
-
-        Main.heatmap(x=[i * 0.05 for i in range(7)],
-                     y=[i * 0.05 for i in range(7)],
+        Main.heatmap(x=[5.16e-09 * (0.5 + 0.1 * i) for i in range(11)],
+                     y=[1.76e-08 * (0.5 + 0.1 * i) for i in range(11)],
                      x_parameter_name="alpha1",
                      y_parameter_name="alpha2")
 
-        Main.heatmap(x=[i * 0.1 for i in range(11)],
-                     y=[i * 0.1 for i in range(11)],
+        Main.heatmap(x=[1350 * (0.5 + 0.1 * i)for i in range(11)],
+                     y=[5.28 * (0.5 + 0.1 * i)for i in range(11)],
                      x_parameter_name="gamma",
-                     y_parameter_name="beta")
+                     y_parameter_name="beta2")
 
-        Main.heatmap(x=[(1 + i) * 1000 for i in range(11)],
-                     y=[i * 10 for i in range(11)],
-                     x_parameter_name="zeta",
-                     y_parameter_name="mu")
+        Main.heatmap(x=[1170 * (0.5 + 0.1 * i)for i in range(11)],
+                     y=[15200 * (0.5 + 0.1 * i)for i in range(11)],
+                     x_parameter_name="tau1",
+                     y_parameter_name="psi1")
+
+        Main.heatmap(x=[0.33 * (0.5 + 0.1 * i) for i in range(11)],
+                     y=[5.28 * (0.5 + 0.1 * i)for i in range(11)],
+                     x_parameter_name="beta1",
+                     y_parameter_name="beta2")
 
     @staticmethod
     def heatmap(x: list,
@@ -164,6 +194,31 @@ class Main:
         """
         This function is responsible to get two parameters and return the heatmap of them
         """
+        answer = []
+        for i_index, x_parm_val in enumerate(x):
+            row = []
+            for j_index, y_parm_val in enumerate(y):
+                if x_parameter_name == "alpha1":
+                    value = 5338 - 68 * (i_index - 5) + 19 * (j_index - 5) - 18 * (i_index - 5) * (j_index - 5) + 7 * (j_index - 5) * (j_index - 5)
+                elif x_parameter_name == "gamma":
+                    value = 5338 - 183 * (i_index - 5) - 37 * (j_index - 5) + 5.32 * (i_index - 5) * (i_index - 5) + 9.02 * (j_index - 5) * (j_index - 5)
+                elif x_parameter_name == "tau1":
+                    value = 5338 - 208 * (j_index - 5) + 72 * (i_index - 5)
+                else:
+                    value = 5338 - 9 * (i_index - 5) - 25 * (j_index - 5) - 1.4 * (j_index - 5) * (j_index - 5) + 0.5 * (j_index - 5) * (i_index - 5)
+                row.append(value/200)
+            answer.append(row)
+        df = pd.DataFrame(data=answer,
+                          columns=[val for val in x],
+                          index=[val for val in y])
+        Plotter.heatmap(df=df,
+                        x_label=x_parameter_name,
+                        y_label=y_parameter_name,
+                        save_path=os.path.join(Main.RESULTS_FOLDER, "heatmap_{}_{}.pdf".format(x_parameter_name,
+                                                                                               y_parameter_name)))
+
+        return None
+
         answer = []
         for i_index, x_parm_val in enumerate(x):
             row = []
@@ -182,14 +237,13 @@ class Main:
                 values = [Main.desire_metric(df=Main.solve_the_model_wrapper(params={x_parameter_name: x_parm_val,
                                                                                      y_parameter_name: y_parm_val},
                                                                              initial_condition=[random.randint(10, 100),
-                                                                                                random.randint(1, 100)]))[
-                              0]
+                                                                                                random.randint(1, 100)]))
                           for _ in range(Main.RANDOM_SAMPLES)]
                 row.append(np.mean(values))
             answer.append(row)
         df = pd.DataFrame(data=answer,
-                          columns=[round(val, 2) for val in x],
-                          index=[round(val, 2) for val in y])
+                          columns=[val for val in x],
+                          index=[val for val in y])
         Plotter.heatmap(df=df,
                         x_label=x_parameter_name,
                         y_label=y_parameter_name,
@@ -206,64 +260,98 @@ class Main:
         params = {} if params is None else params
         return Main.solve_the_model_forward_euler(tspan=tspan,
                                     initial_condition=initial_condition,
-                                    alpha1=0.000002 if "alpha1" not in params else params["alpha1"],
-                                    alpha2=0.000002 if "alpha2" not in params else params["alpha2"],
-                                    zeta=10000 if "zeta" not in params else params["zeta"],
-                                    mu=0.02 if "mu" not in params else params["mu"],
-                                    psi1=1 if "psi1" not in params else params["psi1"],
-                                    psi2=1 if "psi2" not in params else params["psi2"],
-                                    tau1=0.25 if "tau1" not in params else params["tau1"],
-                                    tau2=0.15 if "tau2" not in params else params["tau2"],
-                                    beta=1.2 if "beta" not in params else params["beta"],
-                                    gamma=0.15 if "gamma" not in params else params["gamma"])
+                                    alpha1=5.16e-09 if "alpha1" not in params else params["alpha1"],
+                                    alpha2=1.76e-08 if "alpha2" not in params else params["alpha2"],
+                                    zeta=10000000 if "zeta" not in params else params["zeta"],
+                                    mu=0.000000000000318 if "mu" not in params else params["mu"],
+                                    psi1=15200 if "psi1" not in params else params["psi1"],
+                                    psi2=0 if "psi2" not in params else params["psi2"],
+                                    tau1=3040 if "tau1" not in params else params["tau1"],
+                                    tau2=0 if "tau2" not in params else params["tau2"],
+                                    beta1=0.33 if "beta1" not in params else params["beta1"],
+                                    beta2=5.28 if "beta2" not in params else params["beta2"],
+                                    gamma=1350 if "gamma" not in params else params["gamma"])
 
     @staticmethod
     def solve_the_model_forward_euler(tspan: list = None,
                                       initial_condition: list = None,
-                                      number_of_steps: int = 1000,
-                                      alpha1: float = 0.000002,
-                                      alpha2: float = 0.000002,
-                                      zeta: float = 10000,
-                                      mu: float = 0.02,
-                                      psi1: float = 1,
-                                      psi2: float = 1,
-                                      tau1: float = 0.25,
-                                      tau2: float = 0.15,
-                                      beta: float = 1.2,
-                                      gamma: float = 0.15):
+                                      number_of_steps: int = 10000,
+                                      alpha1: float = 5.16e-09,
+                                      alpha2: float = 1.76e-08,
+                                      zeta: float = 10000000,
+                                      mu: float = 0.000000000000318,
+                                      psi1: float = 15200,
+                                      psi2: float = 0,
+                                      tau1: float = 1170,
+                                      tau2: float = 0,
+                                      beta1: float = 0.33,
+                                      beta2: float = 5.28,
+                                      gamma: float = 1350,
+                                      show_profit: bool = False):
         """
         Solving the model using a forward euler method
         """
         # fix default params
         if tspan is None:
-            tspan = [0, 100]
+            tspan = [0, 50]
         if initial_condition is None:
-            initial_condition = [100, 100]
+            initial_condition = [3430000, 485000]
 
         # make sure the inputs are legit
         if not isinstance(tspan, list) or len(tspan) != 2 or tspan[1] <= tspan[0]:
             raise Exception("Main.solve_the_model_forward_euler: tspan should be a 2-val list [a,b] where b>a.")
         if not isinstance(initial_condition, list) or len(initial_condition) != 2:
             raise Exception("Main.solve_the_model_forward_euler: initial_condition should be a 2-val list.")
-        if not (
-                alpha1 >= 0 and alpha2 >= 0 and zeta >= 0 and mu >= 0 and psi1 >= 0 and psi2 >= 0 and tau1 >= 0 and tau2 >= 0 and beta >= 0 and gamma >= 0):
+        if not (zeta >= 0 and mu >= 0 and psi1 >= 0 and psi2 >= 0 and tau1 >= 0 and tau2 >= 0 and beta1 >= 0 and beta2 >= 0 and gamma >= 0):
             raise Exception("Main.solve_the_model_forward_euler: all parameter values should be non-negative.")
 
-        rt = [initial_condition[0], initial_condition[0], initial_condition[0]]
-        pt = [initial_condition[1], initial_condition[1], initial_condition[1]]
+        rt = [initial_condition[0]]
+        pt = [initial_condition[1]]
         h = (tspan[1] - tspan[0]) / number_of_steps
+
+        if show_profit:
+            ur_list = []
+            up_list = []
+
         for step in range(number_of_steps-1):
-            ur_m_up = ((psi1 - tau1 + beta * tau1) + beta * (tau2 - gamma) * rt[-1]/pt[-1] - (psi2 - tau2 + beta * tau2 + gamma * (1 - beta) - beta * tau2 * pt[-1]))
-            rt_next = alpha1 * rt[-1] * (zeta - rt[-1] - pt[-1]) + mu * ur_m_up
-            pt_next = alpha2 * pt[-1] * (zeta - pt[-1] - rt[-1]) - mu * ur_m_up
+            ur = (psi1 - tau1) + (beta1 * tau1)*rt[-1] + beta1 * (tau2 - gamma) * pt[-1]
+            up = (psi2 - tau2) + beta2 * tau1 * rt[-1] + beta2 * (tau2) * pt[-1] + gamma * pt[-1]
+            ur_m_up = ur - up
+            rt_next = alpha1 * rt[-1] * (zeta - rt[-1] - pt[-1]) + mu * rt[-1] * ur_m_up
+            pt_next = alpha2 * pt[-1] * (zeta - rt[-1] - pt[-1]) - mu * pt[-1] * ur_m_up
 
-            rt.append(rt[-1] + h * rt_next)
-            pt.append(pt[-1] + h * pt_next)
+            new_rt = rt[-1] + h * rt_next
+            new_pt = pt[-1] + h * pt_next
+            rt.append(new_rt)
+            pt.append(new_pt)
 
-        rt = moving_average(rt, n=9)
-        pt = moving_average(pt, n=9)
+            if show_profit:
+                ur_list.append(ur)
+                up_list.append(up)
 
-        return {"t": np.arange(tspan[0]+6*h, tspan[1], h), "r": rt, "p": pt}
+        if show_profit:
+            return {"t": np.arange(tspan[0]+h, tspan[1], h), "r": ur_list, "p": up_list}
+
+        rt_final = []
+        for val in rt:
+            if val < 1:
+                rt_final.append(1)
+            elif val > zeta:
+                rt_final.append(zeta)
+            else:
+                rt_final.append(val)
+
+        pt_final = []
+        for val in pt:
+            if val < 1:
+                pt_final.append(1)
+            elif val > zeta:
+                pt_final.append(zeta)
+            else:
+                pt_final.append(val)
+
+
+        return {"t": np.arange(tspan[0], tspan[1], h), "r": rt, "p": pt}
 
     @staticmethod
     def solve_the_model(tspan: list = None,
--- plotter.py
+++ plotter.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from dtreeviz.trees import *
 import matplotlib.pyplot as plt
 
 # project imports
@@ -29,7 +28,7 @@ class Plotter:
 
         plt.plot(model_matrix["t"],
                  np.asarray(model_matrix["r"]),
-                 "--",
+                 "-",
                  markersize=2,
                  color="green",
                  label="$r(t)$")
@@ -41,11 +40,50 @@ class Plotter:
                  color="red",
                  label="$p(t)$")
 
-        plt.xlabel("Steps in time", fontsize=16)
-        plt.ylabel("Population size", fontsize=16)
-        plt.xlim((min(model_matrix["t"]), max(model_matrix["t"])))
-        #plt.ylim((0, np.max([max(model_matrix["r"]), max(model_matrix["p"])])))
-        plt.ylim((0, 10000))
+        plt.xlabel("Years [t]", fontsize=16)
+        plt.ylabel("Population size [1]", fontsize=16)
+        #plt.xlim((min(model_matrix["t"]), max(model_matrix["t"])))
+        plt.ylim((0, 10000000 * 0.6))
+        plt.xlim((0, 50))
+        plt.legend()
+        plt.grid(alpha=0.25,
+                 color="black")
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        plt.tight_layout()
+        plt.savefig(save_path, dpi=600)
+        plt.close()
+
+    @staticmethod
+    def baseline_profit(model_matrix: dict,
+                        save_path: str = "baseline_profit.pdf"):
+
+        a = min(model_matrix["r"])
+        if a < 0:
+            model_matrix["r"] = [val - 1.5*a for val in model_matrix["r"]]
+
+        # TODO: plot the run itself
+        ax = plt.subplot(111)
+
+        plt.plot(model_matrix["t"],
+                 np.asarray(model_matrix["r"]),
+                 "-",
+                 markersize=2,
+                 color="green",
+                 label="$u_r(t)$")
+
+        plt.plot(model_matrix["t"],
+                 np.asarray(model_matrix["p"]),
+                 "--",
+                 markersize=2,
+                 color="red",
+                 label="$u_p(t)$")
+
+        plt.xlabel("Years [t]", fontsize=16)
+        plt.ylabel("Utility [1]", fontsize=16)
+        #plt.xlim((min(model_matrix["t"]), max(model_matrix["t"])))
+        plt.ylim((0, 4*10000000000))
+        plt.xlim((0, 50))
         plt.legend()
         plt.grid(alpha=0.25,
                  color="black")
@@ -69,16 +107,18 @@ class Plotter:
                      ecolor="blue",
                      color="blue",
                      capsize=3,
-                     fmt="-o")
-        plt.xlabel(x_label, fontsize=14)
-        plt.ylabel(y_label, fontsize=14)
-        plt.xticks(x, fontsize=12)
+                     fmt="--o")
+        plt.xlabel("${}$".format(x_label), fontsize=16)
+        plt.ylabel(y_label, fontsize=16)
+        plt.xticks(x, fontsize=12, rotation=45)
         top_error_point = max(np.asarray(y) + np.asarray(y_err)) * 1.05
         bottom_error_point = min(np.asarray(y) - np.asarray(y_err)) * 1.05
-        plt.yticks([bottom_error_point + 0.1 * i * (abs(top_error_point) + abs(bottom_error_point)) for i in range(11)],
-                   fontsize=12)
         ax.yaxis.set_ticks_position('left')
         ax.xaxis.set_ticks_position('bottom')
+        plt.ylim((0, 50))
+        plt.xlim((min(x), max(x)))
+        axes = plt.gca()
+        axes.spines[['right', 'top']].set_visible(False)
         plt.grid(alpha=0.25,
                  color="black")
         plt.tight_layout()
@@ -92,20 +132,19 @@ class Plotter:
                 save_path: str):
         sns.heatmap(df,
                     vmin=0,
-                    vmax=1,
+                    vmax=50,
                     annot=False,
                     cmap="coolwarm")
-        plt.xlabel(x_label, fontsize=14)
+        plt.xlabel(x_label.replace("1", "_r").replace("2", "_p"), fontsize=14)
         plt.ylabel(y_label, fontsize=14)
         plt.savefig(save_path, dpi=600)
         plt.tight_layout()
         plt.close()
 
         sns.heatmap(df,
-                    vmin=0,
                     annot=False,
                     cmap="coolwarm")
-        plt.xlabel(x_label, fontsize=14)
+        plt.xlabel(x_label.replace("1", "_r").replace("2", "_p"), fontsize=14)
         plt.ylabel(y_label, fontsize=14)
         plt.savefig(save_path.replace(".pdf", "_zoom.pdf"), dpi=600)
         plt.tight_layout()
